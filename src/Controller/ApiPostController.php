f9310e9f167440bc3081c99318a74957180702a2
Damien Baudet <damienbaudet@free.fr>
2020-06-09 18:24:35 +0200

    add method get

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiPostController extends AbstractController
 {
@@ -15,18 +17,8 @@ class ApiPostController extends AbstractController
      * @param PostRepository $postRepository
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(PostRepository $postRepository, NormalizerInterface $normalizer)
+    public function index(PostRepository $postRepository, SerializerInterface $serializer)
     {
-        $posts = $postRepository->findAll();
-
-        $postsNormalises = $normalizer->normalize($posts, null, ['groups' => 'post:read']);
-
-        $json = json_encode($postsNormalises);
-
-        $response = new Response($json, 200, [
-            "Content-Type" => "application/json"
-        ]);
-
-        return $response;
+        return $this->json($postRepository->findAll(), 200, [], ['groups' => 'post:read']);
     }
 }
